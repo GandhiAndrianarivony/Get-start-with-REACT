6aa2a49f6c884412d60c50f831fdf37ab154d484
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-03-23 07:42:41 +0300

    Reusable componet

---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+// Name the component as the jsx file
+const name = "Frederic";
+function Hello() {
+  return (
+    <div>
+      <h1>Hello from a component! {name}</h1>
+    </div>
+  );
+}
+
+export default Hello;
